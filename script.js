a9712d86163b0ff542e175ff115db38f955b067e
Tanvir Mahmud <tmahmud7@my.centennialcollege.ca>
2025-07-29 23:23:59 -0600

    Implement play again logic

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
-
 document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start Guessing... ';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,6 +31,7 @@ document.querySelector('.check').addEventListener('click', function () {
   //When player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
