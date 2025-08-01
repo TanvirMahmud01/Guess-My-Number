4fa8f8c39c6e783184f6520dea19512fa1fe1730
Tanvir Mahmud <tmahmud7@my.centennialcollege.ca>
2025-07-31 21:43:38 -0600

    Add highscore functionality

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let highscore = 0;
 
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
@@ -36,6 +37,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
   //When guess is too high
   else if (guess > secretNumber) {
